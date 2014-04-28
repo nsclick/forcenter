ebc3a203eb5d36b48ad0487180577585a9523521
Ricardo Escobar <zherethor@gmail.com>
2014-04-28 18:34:34 -0300

    Ns Kameleon 0.3

--- wp-content/themes/nskameleon/header.php
+++ wp-content/themes/nskameleon/header.php
@@ -34,6 +34,13 @@
 		<link rel="stylesheet" media="only screen and (max-width: 1024px) and (min-width: 769px)" href="<?php echo get_template_directory_uri(); ?>/assets/css/1024.css">
 		<link rel="stylesheet" media="only screen and (max-width: 768px) and (min-width: 481px)" href="<?php echo get_template_directory_uri(); ?>/assets/css/768.css">
 		<link rel="stylesheet" media="only screen and (max-width: 480px)" href="<?php echo get_template_directory_uri(); ?>/assets/css/480.css">
+		
+		<!-- Forcenter Camouflage -->
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/camouflage/forcenter/css/forcenter.css" media="all">
+		<link rel="stylesheet" media="only screen and (max-width: 1024px) and (min-width: 769px)" href="<?php echo get_template_directory_uri(); ?>/camouflage/forcenter/css/1024.css">
+		<link rel="stylesheet" media="only screen and (max-width: 768px) and (min-width: 481px)" href="<?php echo get_template_directory_uri(); ?>/camouflage/forcenter/css/768.css">
+		<link rel="stylesheet" media="only screen and (max-width: 480px)" href="<?php echo get_template_directory_uri(); ?>/camouflage/forcenter/css/480.css">
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 
 		<!-- All JavaScript at the bottom, except for Modernizr which enables HTML5 elements and feature detects -->
 		<script src="<?php echo get_template_directory_uri(); ?>/assets/js/modernizr-2.5.3-min.js"></script>
@@ -47,25 +54,37 @@
 		<div id="wrapper">
 			<div id="header">
 				<div class="content">
+					<div class="spacer">&nbsp;</div><div class="spacer">&nbsp;</div>
 					<div class="section group">
-						<div class="col span_8_of_12">
+						<div class="col span_6_of_12">
 							<div id="logo-container">
-								<a href="<?php echo get_site_url(); ?>/" id="logo"> <img src="<?php echo get_template_directory_uri(); ?>/assets/images/nskameleon_logo.png" alt="logo"/> </a>
+								<a href="<?php echo get_site_url(); ?>/" id="logo"> <img src="<?php echo get_template_directory_uri(); ?>/camouflage/forcenter/images/logo-ford.png" alt="logo"/> </a>
 							</div>
 						</div>
-						<div class="col span_4_of_12">
+						<div class="col span_6_of_12">
 							<div id="toolbar">
-								<a href="#" class="tool"><i class="icon-wrench"></i> Tool</a>
-								<a href="#" class="tool"><i class="icon-wrench"></i> Tool</a>
-								<a href="#" class="tool"><i class="icon-wrench"></i> Tool</a>
-								<a href="#" class="tool"><i class="icon-wrench"></i> Tool</a>
+								<div class="toolbar-box">
+									<span><img src="<?php echo get_template_directory_uri(); ?>/camouflage/forcenter/images/logo-forcenter350.png" alt="Forcenter"/></span>
+									<div class="sea">
+										<input type="text" /><button type="submit"><img style="vertical-align:middle" src="<?php echo get_template_directory_uri(); ?>/camouflage/forcenter/images/ico-search.png" alt="Forcenter"/></button>
+									</div>
+									<div class="sel">
+										<select>
+											<option>Tienes seleccionados 0 veh&iacute;culos</option>
+										</select>
+									</div>
+									<a href="#"><img style="vertical-align:middle" src="<?php echo get_template_directory_uri(); ?>/camouflage/forcenter/images/ico-mail.png" alt="Forcenter"/> Contacto</a>
+								</div>
 							</div>
 						</div>
 					</div>
+					<div class="spacer">&nbsp;</div><div class="spacer">&nbsp;</div>
 					<div class="section group">
 						<div class="col span_12_of_12">
 							<div id="menu">
 								<ul id="nav">
+									<li class="nav-link"><a href="#">menu 1</a></li>
+									<li class="nav-link"><a href="#">menu 1</a></li>
 									<li class="nav-link"><a href="#">menu 1</a></li>
 									<li class="nav-link"><a href="#">menu 1</a></li>
 									<li class="nav-link"><a href="#">menu 1</a></li>
--- wp-content/themes/nskameleon/index.php
+++ wp-content/themes/nskameleon/index.php
@@ -4,24 +4,75 @@
  * First NSClick responsive Wordpress theme
  * 
  */
-
 get_header(); ?>
 <div id="main-content">
 	<div class="content">
 		<div class="section group">
 			<div class="col span_12_of_12">
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/nskameleon-slider.png" alt="NSkameleon"/>
-				<h1>Bienvenido a NSkameleon</h1>
-				<h2>El primer Tema para Wordpress, visible en todo tipo de dispositivos, creado por NSclick.</h2>
-				<p>Donec ut bibendum nibh. Quisque placerat aliquam bibendum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent ultricies nisl et nisi scelerisque, ac pulvinar ante mollis. Nam varius auctor ornare. Maecenas sit amet porta ligula. Praesent ut justo convallis, porta orci eu, suscipit neque. Sed aliquet ipsum a turpis pulvinar sagittis. In eget mauris sapien. Phasellus quis ullamcorper odio, vel congue nulla. Donec interdum, mauris at bibendum vehicula, nibh orci accumsan ante, facilisis tempus sapien dui at mi. Suspendisse sit amet sollicitudin erat.</p>
+				<div id="sliderhome">
+					<?php echo do_shortcode('[widgetkit id=10]') ?>
+				</div>
 			</div>
 		</div>
 		<div class="section group">
-			<div class="col span_6_of_12">
-				<p>Donec ut bibendum nibh. Quisque placerat aliquam bibendum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent ultricies nisl et nisi scelerisque, ac pulvinar ante mollis. Nam varius auctor ornare. Maecenas sit amet porta ligula. Praesent ut justo convallis, porta orci eu, suscipit neque. Sed aliquet ipsum a turpis pulvinar sagittis. In eget mauris sapien. Phasellus quis ullamcorper odio, vel congue nulla. Donec interdum, mauris at bibendum vehicula, nibh orci accumsan ante, facilisis tempus sapien dui at mi. Suspendisse sit amet sollicitudin erat.</p>
+			<div class="col span_12_of_12">
+				<div class="hr">&nbsp;</div>
 			</div>
-			<div class="col span_6_of_12">
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/nskameleon-slider.png" alt="NSkameleon"/>
+		</div>
+		<div class="section group">
+			<div class="col span_4_of_12">
+				<a href="#" class="box-a"><img src="<?php echo get_template_directory_uri(); ?>/camouflage/forcenter/images/box1.png" alt="forcenter"/></a>
+			</div>
+			<div class="col span_4_of_12">
+				<a href="#" class="box-b">
+					<span class="title">Servicio <b>T&eacute;cnico</b></span>
+					<span class="link">Ver m&aacute;s</span>
+					<img src="<?php echo get_template_directory_uri(); ?>/camouflage/forcenter/images/box2-a.png" alt="forcenter"/>
+				</a>
+			</div>
+			<div class="col span_4_of_12">
+				<a href="#" class="box-b">
+					<span class="title">Cotiza aqu&iacute; tu <b>Ford</b></span>
+					<span class="link2">Cotiza ahora</span>
+					<img src="<?php echo get_template_directory_uri(); ?>/camouflage/forcenter/images/box2-b.png" alt="forcenter"/>
+				</a>
+			</div>
+		</div>
+		<div class="section group">
+			<div class="col span_12_of_12">
+				<div class="hr">&nbsp;</div>
+			</div>
+		</div>
+		<div class="section group">
+			<div class="col span_4_of_12">
+				<div class="box-c">
+					<a href="#" class="title">
+						<span>Financiamiento</span>
+						<span><img src="<?php echo get_template_directory_uri(); ?>/camouflage/forcenter/images/box-arrow.png" alt="forcenter"/></span>
+					</a>
+					<span class="bg">&nbsp;</span>
+					<img class="bg" src="<?php echo get_template_directory_uri(); ?>/camouflage/forcenter/images/box3-a.png" alt="forcenter"/>
+				</div>
+			</div>
+			<div class="col span_4_of_12">
+				<div class="box-c">
+					<a href="#" class="title">
+						<span>Seguros</span>
+						<span><img src="<?php echo get_template_directory_uri(); ?>/camouflage/forcenter/images/box-arrow.png" alt="forcenter"/></span>
+					</a>
+					<span class="bg">&nbsp;</span>
+					<img class="bg" src="<?php echo get_template_directory_uri(); ?>/camouflage/forcenter/images/box3-b.png" alt="forcenter"/>
+				</div>
+			</div>
+			<div class="col span_4_of_12">
+				<div class="box-c">
+					<a href="#" class="title">
+						<span>Autos<br />Seminuevos</span>
+						<span><img src="<?php echo get_template_directory_uri(); ?>/camouflage/forcenter/images/box-arrow.png" alt="forcenter"/></span>
+					</a>
+					<span class="bg">&nbsp;</span>
+					<img class="bg" src="<?php echo get_template_directory_uri(); ?>/camouflage/forcenter/images/box3-c.png" alt="forcenter"/>
+				</div>
 			</div>
 		</div>
 		<div class="spacer">&nbsp;</div>
