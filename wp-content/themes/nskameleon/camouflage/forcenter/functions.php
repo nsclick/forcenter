9c16ca18f4fecd5a61c7b1e54c257f44f987eb7b
CesarReyes <creyes@nsclick.cl>
2014-04-30 14:11:10 -0400

    Custom posts

--- wp-content/themes/nskameleon/camouflage/forcenter/functions.php
+++ wp-content/themes/nskameleon/camouflage/forcenter/functions.php
@@ -35,3 +35,99 @@ add_action( 'wp_enqueue_scripts', 'nsk_fc_scripts' );
 /*
  * Loading the widgets*/
  require_once (ACTIVE_CAMOUFLAGE_PATH . '/widgets.php');
+
+
+add_action('init', 'register_cpt_modelo');
+
+function register_cpt_modelo()
+{
+	$labels = array(
+		'name' => _x('Modelos', 'modelo') ,
+		'singular_name' => _x('Modelo', 'modelo') ,
+		'add_new' => _x('Add New', 'modelo') ,
+		'add_new_item' => _x('Add New Modelo', 'modelo') ,
+		'edit_item' => _x('Edit Modelo', 'modelo') ,
+		'new_item' => _x('New Modelo', 'modelo') ,
+		'view_item' => _x('View Modelo', 'modelo') ,
+		'search_items' => _x('Search Modelos', 'modelo') ,
+		'not_found' => _x('No modelos found', 'modelo') ,
+		'not_found_in_trash' => _x('No modelos found in Trash', 'modelo') ,
+		'parent_item_colon' => _x('Parent Modelo:', 'modelo') ,
+		'menu_name' => _x('Modelos', 'modelo') ,
+	);
+	$args = array(
+		'labels' => $labels,
+		'hierarchical' => true,
+		'description' => 'Modelos Ford',
+		'supports' => array(
+			'title',
+			'editor',
+			'thumbnail',
+			'page-attributes'
+		) ,
+		'taxonomies' => array(
+			'page-category'
+		) ,
+		'public' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'menu_position' => 5,
+		'show_in_nav_menus' => true,
+		'publicly_queryable' => true,
+		'exclude_from_search' => false,
+		'has_archive' => true,
+		'query_var' => true,
+		'can_export' => true,
+		'rewrite' => true,
+		'capability_type' => 'page'
+	);
+	register_post_type('modelo', $args);
+}
+
+
+add_action('init', 'register_cpt_version');
+
+function register_cpt_version()
+{
+	$labels = array(
+		'name' => _x('Versiones', 'version') ,
+		'singular_name' => _x('Versión', 'version') ,
+		'add_new' => _x('Add New', 'version') ,
+		'add_new_item' => _x('Add New Versión', 'version') ,
+		'edit_item' => _x('Edit Versión', 'version') ,
+		'new_item' => _x('New Versión', 'version') ,
+		'view_item' => _x('View Versión', 'version') ,
+		'search_items' => _x('Search Versiones', 'version') ,
+		'not_found' => _x('No modelos found', 'version') ,
+		'not_found_in_trash' => _x('No Versión found in Trash', 'version') ,
+		'parent_item_colon' => _x('Parent Modelo:', 'version') ,
+		'menu_name' => _x('Versiones', 'version') ,
+	);
+	$args = array(
+		'labels' => $labels,
+		'hierarchical' => true,
+		'description' => 'Versiones Ford',
+		'supports' => array(
+			'title',
+			'editor',
+			'thumbnail',
+			'page-attributes'
+		) ,
+		'taxonomies' => array(
+			'page-category'
+		) ,
+		'public' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'menu_position' => 5,
+		'show_in_nav_menus' => true,
+		'publicly_queryable' => true,
+		'exclude_from_search' => false,
+		'has_archive' => true,
+		'query_var' => true,
+		'can_export' => true,
+		'rewrite' => true,
+		'capability_type' => 'page'
+	);
+	register_post_type('version', $args);
+}
