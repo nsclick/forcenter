d0bba29aa07d3a27148115b8ca16c3f64a3eda1c
Ricardo Escobar <zherethor@gmail.com>
2014-05-26 17:57:55 -0400

    Arreglos
    compra inteligente

--- wp-content/themes/nskameleon/camouflage/forcenter/shortcodes/compra_inteligente.php
+++ wp-content/themes/nskameleon/camouflage/forcenter/shortcodes/compra_inteligente.php
@@ -1,11 +1,17 @@
 <?php
 
-//[contacto]
+//[compra_inteligente]
 function ns_compra_inteligente_shortcode( $atts ) {
 	ob_start();
 	?> 
 		<form method="post" accept-charset="utf-8" action="" />
 		<div class="cotizador">
+			<p>La Compra Inteligente es un sistema de financiamiento que puede ser a 24 ó a 36 meses, para las personas que deseen renovar su vehículo cada 2 ó 3 años, aprovechando siempre de disfrutar la mejor vida útil de su automóvil.</p>
+			<p>Con la Compra Inteligente puedes disfrutar de múltiples beneficios:COMODIDAD: De disfrutar un auto nuevo cada 2 ó 3 años.SEGURIDAD: De contar con un Valor Futuro Mínimo Garantizado para su auto al cabo de los dos o tres años, eliminando así los riesgos y las incomodidades de la reventa.AHORRO: Por mantener el automóvil durante el mejor período de su vida útil, minimizando los gastos de mantención y reparaciones, beneficiándote de la garantía del fabricante.PROPIEDAD: El vehículo queda a tu nombre desde el primer día.</p>
+			<p>¿Cómo funciona?</p>
+			<p>Compra Inteligente es un sistema de financiamiento a 24 ó 36 meses, creado para quienes desean renovar su vehículo cada dos o tres años.</p>
+			<p>Con Compra Inteligente, sólo tienes que elegir el modelo que más se ajusta a tus necesidades (*) y FORUM te garantiza el valor que, como mínimo, tu automóvil tendrá dentro de dos o tres años. Es lo que se denomina el Valor Futuro Mínimo Garantizado (VFMG) y que equivale a la cuota N° 25 ó 37 de tu crédito. Este valor se resta, junto con el pie inicial, del precio del vehículo, y entonces sólo pagas esta diferencia más el costo del dinero en 24 cuotas mensuales para el caso de 2 años o 36 cuotas para renovar cada 3 años.Al finalizar el contrato eliges entre tres alternativas la que más te convenga: cambiar el auto por uno nuevo, quedarte con él o devolverlo.</p>
+			<p>(*) Sólo para vehículos de pasajeros (excluye los vehículos comerciales).</p>
 			<div id="step3" class="activ servicio">
 				<div class="selector">
 					<div class="title">
